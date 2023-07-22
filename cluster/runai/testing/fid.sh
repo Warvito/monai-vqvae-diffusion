b415524be16103e4558caff060c83d357c3e5847
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-07-22 09:27:20 +0100

    Fix sampling

--- cluster/runai/testing/fid.sh
+++ cluster/runai/testing/fid.sh
@@ -1,11 +1,38 @@
 seed=42
-sample_dir="/project/outputs/samples_unconditioned/"
+sample_dir="/project/outputs/samples_unconditioned_ldm/"
 test_ids="/project/outputs/ids/test.tsv"
 num_workers=8
 batch_size=16
 
 runai submit \
   --name  mimic-fid \
+  --image aicregistry:5000/wds20:ldm_vqvae \
+  --backoff-limit 0 \
+  --gpu 1 \
+  --cpu 4 \
+  --large-shm \
+  --run-as-user \
+  --host-ipc \
+  --project wds20 \
+  --volume /nfs/home/wds20/projects/monai-vqvae-diffusion/:/project/ \
+  --volume /nfs/home/wds20/datasets/MIMIC-CXR-JPG_v2.0.0/:/data/ \
+  --command -- bash /project/src/bash/start_script.sh \
+      python3 /project/src/python/testing/compute_fid.py \
+        seed=${seed} \
+        sample_dir=${sample_dir} \
+        test_ids=${test_ids} \
+        batch_size=${batch_size} \
+        num_workers=${num_workers}
+
+
+seed=42
+sample_dir="/project/outputs/samples_unconditioned_transformer/"
+test_ids="/project/outputs/ids/test.tsv"
+num_workers=8
+batch_size=16
+
+runai submit \
+  --name  mimic-fid-transformer \
   --image aicregistry:5000/wds20:ldm_mimic \
   --backoff-limit 0 \
   --gpu 1 \
@@ -16,9 +43,10 @@ runai submit \
   --project wds20 \
   --volume /nfs/home/wds20/projects/monai-vqvae-diffusion/:/project/ \
   --volume /nfs/home/wds20/datasets/MIMIC-CXR-JPG_v2.0.0/:/data/ \
-  --command -- python3 /project/src/python/testing/compute_fid.py \
-      --seed=${seed} \
-      --sample_dir=${sample_dir} \
-      --test_ids=${test_ids} \
-      --batch_size=${batch_size} \
-      --num_workers=${num_workers}
+  --command -- bash /project/src/bash/start_script.sh \
+      python3 /project/src/python/testing/compute_fid.py \
+        seed=${seed} \
+        sample_dir=${sample_dir} \
+        test_ids=${test_ids} \
+        batch_size=${batch_size} \
+        num_workers=${num_workers}
--- cluster/runai/testing/sampling_unconditioned_transformer.sh
+++ cluster/runai/testing/sampling_unconditioned_transformer.sh
@@ -6,25 +6,29 @@ transformer_config_file_path="/project/configs/transformer/transformer_v0.yaml"
 start_seed=0
 stop_seed=1000
 
-runai submit \
-  --name  sampling-mimic-transformer \
-  --image aicregistry:5000/wds20:ldm_vqvae \
-  --backoff-limit 0 \
-  --gpu 1 \
-  --cpu 4 \
-  --memory-limit 256G \
-  --large-shm \
-  --run-as-user \
-  --node-type "A100" \
-  --host-ipc \
-  --project wds20 \
-  --volume /nfs/home/wds20/projects/monai-vqvae-diffusion/:/project/ \
-  --command -- bash /project/src/bash/start_script.sh \
-      python3 /project/src/python/testing/sample_images_transformer.py \
-        output_dir=${output_dir} \
-        stage1_path=${stage1_path} \
-        transformer_path=${transformer_path} \
-        stage1_config_file_path=${stage1_config_file_path} \
-        transformer_config_file_path=${transformer_config_file_path} \
-        start_seed=${start_seed} \
-        stop_seed=${stop_seed}
+for i in {0..9}; do
+  start_index=$((i*100))
+  stop_index=$(((i+1)*100))
+  runai submit \
+    --name  sampling-mimic-transformer-${i} \
+    --image aicregistry:5000/wds20:ldm_vqvae \
+    --backoff-limit 0 \
+    --gpu 1 \
+    --cpu 4 \
+    --memory-limit 256G \
+    --large-shm \
+    --run-as-user \
+    --node-type "A100" \
+    --host-ipc \
+    --project wds20 \
+    --volume /nfs/home/wds20/projects/monai-vqvae-diffusion/:/project/ \
+    --command -- bash /project/src/bash/start_script.sh \
+        python3 /project/src/python/testing/sample_images_transformer.py \
+          output_dir=${output_dir} \
+          stage1_path=${stage1_path} \
+          transformer_path=${transformer_path} \
+          stage1_config_file_path=${stage1_config_file_path} \
+          transformer_config_file_path=${transformer_config_file_path} \
+          start_seed=${start_seed} \
+          stop_seed=${stop_seed}
+done
